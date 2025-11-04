7c5f39a4901d379ac2cc9757c4ef4b374d527d34
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-04 10:40:19 -0500

    feat(exception-handler): add global exception handler for rest controllers with localized error messages.

---
+++ src/main/java/com/acme/catchup/platform/shared/interfaces/rest/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.acme.catchup.platform.shared.interfaces.rest;
+
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.Locale;
+
+/**
+ * Global exception handler for the application.
+ * @summary
+ * This class handles exceptions thrown by REST controllers.
+ * It includes support for localized error messages.
+ * @since 1.0
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    MessageSource messageSource;
+
+    public GlobalExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    /**
+     * Handles MethodArgumentNotValidException.
+     * @param exception The {@link MethodArgumentNotValidException} exception to handle
+     * @param locale The {@link Locale} locale to use for error messages
+     * @return The {@link ErrorResponse} error response
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ErrorResponse handleException(MethodArgumentNotValidException exception, Locale locale) {
+        String message = exception.getFieldErrors().stream()
+                .map(fieldError -> messageSource.getMessage(fieldError, locale))
+                .reduce(messageSource.getMessage("errors.found", null,
+                        locale), String::concat);
+        return ErrorResponse.create(
+                exception,
+                HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value()),
+                message
+        );
+    }
+
+    /**
+     * Handles IllegalArgumentException.
+     * @param exception The {@link IllegalArgumentException} exception to handle
+     * @param locale The {@link Locale} locale to use for error messages
+     * @return The {@link ErrorResponse} error response
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ErrorResponse handleException(IllegalArgumentException exception, Locale locale) {
+        String message = messageSource.getMessage(exception.getMessage(), null, locale);
+        return ErrorResponse.create(exception, HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value()), message);
+    }
+
+}
