aafaf3dc66257a4cd8e8c709512dec68173f3621
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-22 11:22:53 -0500

    fix: update main class and main method.

--- src/main/java/com/acme/catchup/platform/CatchUpPlatformApplication.java
+++ src/main/java/com/acme/catchup/platform/CatchUpPlatformApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
- * CatchUpPlatformApplication
- *
- * @summary
- * The main class of the CatchUp Platform application.
- * It is responsible for starting the Spring Boot application.
- * It also enables JPA auditing.
+ * Main entry point for the CatchUp Platform Spring Boot application.
+ * Enables JPA auditing support for auditing entity fields.
  *
  * @since 1.0
  */
@@ -18,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class CatchUpPlatformApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(CatchUpPlatformApplication.class, args);
     }
 
