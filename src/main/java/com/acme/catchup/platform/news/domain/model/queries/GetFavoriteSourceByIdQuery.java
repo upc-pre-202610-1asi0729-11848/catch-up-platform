5f0dfad155ff9dabbf4fd5204d3f0e307ae0baa4
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-22 05:26:49 -0500

    fix: validate `id` in `get favorite source by id query` to ensure it is positive and non-null.

--- src/main/java/com/acme/catchup/platform/news/domain/model/queries/GetFavoriteSourceByIdQuery.java
+++ src/main/java/com/acme/catchup/platform/news/domain/model/queries/GetFavoriteSourceByIdQuery.java
@@ -7,8 +7,8 @@ package com.acme.catchup.platform.news.domain.model.queries;
  */
 public record GetFavoriteSourceByIdQuery(Long id) {
     public GetFavoriteSourceByIdQuery {
-        if (id == null) {
-            throw new IllegalArgumentException("Id cannot be null");
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("favorite.source.error.id.invalid");
         }
     }
 }
